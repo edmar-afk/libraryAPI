f3649b66a0183faa0b18c03b24dce3a36cf6ba76
project2479 <jaywrsnp6@gmail.com>
2024-04-17 11:49:33 +0800

    new UI

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,28 @@
+from django import forms
 from django.contrib import admin
 from .models import Librarian, Visit, Booking
 
+class CustomTimeInput(forms.TimeInput):
+    input_type = 'time'
+
+    def __init__(self, attrs=None, format='%I:%M %p'):
+        super().__init__(attrs=attrs, format=format)
+
+class BookingAdminForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        widgets = {
+            'startDatetime': CustomTimeInput(),
+            'endDatetime': CustomTimeInput(),
+        }
+
+class BookingAdmin(admin.ModelAdmin):
+    form = BookingAdminForm
+    list_display = ['event', 'month', 'startDatetime', 'endDatetime']
+
+admin.site.register(Booking, BookingAdmin)
+
+
 admin.site.register(Librarian)
 admin.site.register(Visit)
-admin.site.register(Booking)
--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     "corsheaders",
+    
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -151,3 +153,37 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Library Admin",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Library",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Library",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": "books/img/logo.png",
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": None,
+
+    # Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": None,
+
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to the library",
+
+    # Copyright on the footer
+    "copyright": "Acme Library Ltd",
+
+}
